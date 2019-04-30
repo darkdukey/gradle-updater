f0b631c75426492e7dec0bf3eaf645f758b62150
Nite <nite.luo@samsung.com>
2019-04-30 16:54:14 -0700

    Implement simple upgrade

--- gradle_update.py
+++ gradle_update.py
@@ -3,14 +3,15 @@ import os
 from common import *
 
 def update_gradle(root_path, gradle_version):
-    for root, dirs, files in os.walk(root_path, topdown=False):
+    for root, dirs, files in os.walk(root_path, topdown=True):
         for f in files:
             if "gradlew" == f:
                 print "root:" + root
                 print "file:" + f
-                os.system("gradle wrapper -d " + root + " --gradle-version=" + gradle_version + " --distribution-type=all")
-        # for d in dirs:
-        #     print(os.path.join(root, d))
+                # abs_path = os.path.normpath(root)
+                abs_path = os.path.abspath(os.path.join(root_path, root))
+                os.chdir(abs_path)
+                os.system("gradlew wrapper --gradle-version=" + gradle_version + " --distribution-type=all")
 
 def main():
     # find all android studio projects under current path
@@ -18,7 +19,7 @@ def main():
     parser = OptionParser(usage='usage: %prog root_directory -v 5.4.1')
     parser.add_option('-v', '--version',dest='version', help="Version for gradle")
     (opts, args) = parser.parse_args()
-    
+
     if not opts.version:
         parser.error('please specify gradle version with -v')
 
